99d3eda02e8f3ea0884a4b88331d55f487943f52
pujanb96 <pujanb96@gmail.com>
2019-12-22 19:10:35 -0400

    Dashboard modal added

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -15,50 +15,72 @@ const customStyles = {
 
 Modal.setAppElement("#root");
 
-const Dashboard = props => {
-  this.openModal = this.openModal.bind(this);
-  this.afterOpenModal = this.afterOpenModal.bind(this);
-  this.closeModal = this.closeModal.bind(this);
+export default class Dashboard extends React.Component {
+  constructor() {
+    super();
 
-  function openModal() {
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
     this.setState({ modalIsOpen: true });
   }
 
-  function afterOpenModal() {
+  afterOpenModal() {
     // references are now sync'd and can be accessed.
     this.subtitle.style.color = "#f00";
   }
 
-  function closeModal() {
+  closeModal() {
     this.setState({ modalIsOpen: false });
   }
 
-  return (
-    <div class="ui secondary vertical pointing menu">
-      <a class="active item" onClick={this.openModal}>
-        Expenses
-        <div class="ui left pointing label">1</div>
-      </a>
-      <a class="item">
-        Incomes
-        <div class="ui label">51</div>
-      </a>
-      <a class="item">
-        Accounts
-        <div class="ui label">1</div>
-      </a>
-      <div class="item">
-        <div class="ui transparent icon input">
-          <input type="text" placeholder="Search trasactions..." />
-          <i class="search icon"></i>
+  render() {
+    return (
+      <div class="ui secondary vertical pointing menu">
+        <a class="active item" onClick={this.openModal}>
+          Expenses
+          <div class="ui left pointing label">1</div>
+        </a>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 ref={subtitle => (this.subtitle = subtitle)}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+        <a class="item">
+          Incomes
+          <div class="ui label">51</div>
+        </a>
+        <a class="item">
+          Accounts
+          <div class="ui label">1</div>
+        </a>
+        <div class="item">
+          <div class="ui transparent icon input">
+            <input type="text" placeholder="Search trasactions..." />
+            <i class="search icon"></i>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-Dashboard.defaultProps = {
-  modalIsOpen: false
-};
-
-export default Dashboard;
+    );
+  }
+}
