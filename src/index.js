1df5df3a501d00eaacf52baaa5a5dae78269a1a5
pujanb96 <pujanb96@gmail.com>
2019-12-22 02:53:28 -0400

    BASE SETUP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  //state = {};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //componentDidMount() {}
+
+  // renderContent() {
+  // <div className="ui middle aligned center aligned grid"></div>;
+  //}
+
+  render() {
+    return <div>TEST</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
