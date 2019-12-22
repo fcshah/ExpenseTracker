ac48a70b278d5918c8d1ac3c7d995447f85077c6
Fenil Shah <36612912+fcshah@users.noreply.github.com>
2019-12-22 14:19:15 -0400

    RouterInitial

---
+++ src/assets/js/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import elliot from "../img/elliot.jpg";
+
+const Home = props => {
+  return (
+    <div className="ui fluid card">
+      <h4>Jay Hanuman!!!</h4>
+      
+    </div>
+  );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import HomeCard from "./assets/js/card";
-import LoginForm from "./assets/js/login";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+//import App from './assets/js/login'
+import Home from './assets/js/Home'
+//import signup from './assets/js/signup'
+import LoginForm from './assets/js/login'
+//import HomeCard from './assets/js/card'
 
-class App extends React.Component {
-  state = {};
-
-  //componentDidMount() {}
-
-  // renderContent() {
-  // }
-
-  render() {
-    return (
-      <div className="ui container">
-        <div className="ui secondary red raised middle aligned center aligned grid text container segment">
-          <div className="column">
-            <HomeCard />
-
-            <LoginForm />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" component={LoginForm} />
+      <Route path="/home" component={Home} />
+      {/* <Route path="/contact" component={signup} /> */}
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'));
